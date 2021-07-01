555b0343818d9f2d32df501396c229ec4aacb1da
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 14:00:34 +0200

    Update candlescan_settings.js

--- candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.js
+++ candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Candlescan Settings', {
-	// refresh: function(frm) {
-
-	// }
+	 refresh: function(frm) {
+		frm.add_custom_button("Reload scanners",
+							() => {
+								frappe.call({
+									method: 'candlescan.candlescan.doctype.candlescan_settings.candlescan_settings.start_scanners',
+									args: { },
+									callback(r) {
+										
+									}
+								});
+							});
+	 }
 });
