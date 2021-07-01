58bdeba595bef5867b1ddb3edf9eb40dcbd2402e
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 15:47:52 -0400

    Update candlescan_socket.js

--- candlescan/candlescan_socket.js
+++ candlescan/candlescan_socket.js
@@ -16,33 +16,23 @@ server.listen(9001);
 io.on('connection',function(socket){
 	//console.log("connected");
 	socket.emit('candlesocket',"Welcome");
-	socket.on('candlesocket',function(data){
-		
-		console.log('getting msg from web',data);
-
-	});
+	//socket.on('candlesocket',function(data){
+	//	console.log('getting msg from web',data);
+	//});
 })
 
 
 
 var conf = get_conf();
-
-//var subscriber = redis.createClient(conf.redis_socketio || conf.redis_async_broker_port);
-
 var subscriber = get_redis_subscriber();
-
 subscriber.on('message',function(channel,message){
 
 	if(channel=='candlesocket'){
 		message = JSON.parse(message);
-		console.log('channel',channel);
-		//console.log('socket MESSAGE',message);
-		if(message) {
-			console.log("Broadcasting",message);
-			io.sockets.emit('candlesocket',message);
+		if(message.scanner_id) {
+			io.sockets.emit(message.scanner_id,message);
 		}
 	}
 
 });
-//subscriber.subscribe('events');
 subscriber.subscribe('candlesocket');
