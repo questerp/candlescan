f7eba8d2a59f0b1954f0280447eb4777f9bb3db6
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 13:40:27 +0200

    Update candlescan_settings.py

--- candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.py
+++ candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.py
@@ -8,6 +8,7 @@ from frappe.model.document import Document
 from frappe.realtime import get_redis_server
 from frappe.utils.background_jobs import enqueue,get_redis_conn,get_jobs,enqueue_doc
 from rq.job import Job
+from rq.registry import StartedJobRegistry
 
 
 class CandlescanSettings(Document):
@@ -16,14 +17,11 @@ class CandlescanSettings(Document):
 
     def start_scanners(self):
         redis = get_redis_conn()
-        #registry = StartedJobRegistry('long', connection=redis)
-        #running_job_ids = registry.get_job_ids()  # Jobs which are exactly running. 
+        registry = StartedJobRegistry('long', connection=redis)
+        running_job_ids = registry.get_job_ids()  # Jobs which are exactly running. 
 
-        #for job in running_job_ids:
-        #    job = Job.fetch(job, connection=redis)
-        #    print("job: %s" % job)
-        #    print('Status: %s' % job.get_status())
-            
+        for job in running_job_ids:
+            print("job: %s" % job)
             
         scanners = frappe.db.sql(""" select name,active,job_id,scanner,method from `tabCandlescan scanner` """,as_dict=True)
         for s in scanners:
