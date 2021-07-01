b8700805aea17399973543a19c67bbdbac3e608d
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 13:30:19 +0200

    Update candlescan_settings.py

--- candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.py
+++ candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.py
@@ -7,7 +7,7 @@ import frappe
 from frappe.model.document import Document
 from frappe.realtime import get_redis_server
 from frappe.utils.background_jobs import enqueue,get_redis_conn,get_jobs,enqueue_doc
-from rq.registry import StartedJobRegistry
+from rq.job import Job
 
 
 class CandlescanSettings(Document):
@@ -16,21 +16,31 @@ class CandlescanSettings(Document):
 
     def start_scanners(self):
         redis = get_redis_conn()
-        registry = StartedJobRegistry('long', connection=redis)
-        running_job_ids = registry.get_job_ids()  # Jobs which are exactly running. 
+        #registry = StartedJobRegistry('long', connection=redis)
+        #running_job_ids = registry.get_job_ids()  # Jobs which are exactly running. 
 
-        for job in running_job_ids:
-            print("job: %s" % job)
+        #for job in running_job_ids:
+        #    job = Job.fetch(job, connection=redis)
+        #    print("job: %s" % job)
+        #    print('Status: %s' % job.get_status())
+            
             
         scanners = frappe.db.sql(""" select name,active,job_id,scanner,method from `tabCandlescan scanner` """,as_dict=True)
         for s in scanners:
             print("scanner: %s" % s.scanner)
-            print("active: %s" % s.active)
-
-            print(s.job_id)
+            print("active: %s" % s.job_id)
+            if s.job_id:
+                job = Job.fetch(s.job_id, connection=redis)
+                print('Stopping job, Status: %s' % job.get_status())
+                job.cancel()
+                job.delete()
+                
             if s.active:
                 print("Starting")
                 #enqueue_doc(s.scanner, name=s.scanner, method="start", queue='long')
                 q = enqueue(s.method, queue='long', job_name=s.job_id)
-                print(q)
+                id = q.get_id()
+                print(id)
+                frappe.db.sql("""UPDATE set job_id='%s' from `tabCandlescan scanner` where name=%s""" % (id,s.name))
+
                 
