53033f31b64e389a96270650cafdb0dfc5f7e686
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 12:11:31 +0200

    Update premarket_scanner.py

--- candlescan/candlescan/doctype/premarket_scanner/premarket_scanner.py
+++ candlescan/candlescan/doctype/premarket_scanner/premarket_scanner.py
@@ -10,24 +10,11 @@ from frappe.model.document import Document
 from frappe.realtime import get_redis_server
 
 class PremarketScanner(Document):
-    def on_update(self):
-        #if self.active:
-            #enqueue_doc(self.doctype,self.name,"start_scanner",is_async=False)
-            #frappe.msgprint(job.id)
-        frappe.db.set_value("Premarket Scanner",None,"active",self.active)
-        enqueue_doc(self.doctype,self.name,"start_scanner",is_async=False)
-    
-    def start_scanner(self):        
-        if self.active:
-            redis = get_redis_server()
-            val = 1
-            active = 1
-            while(True):
-                if (val % 10) == 0:
-                    active = frappe.db.get_value("Premarket Scanner",self.name,"active")
-                if not active:
-                    break
-                val=val+1 
-                time.sleep(self.update_ms/1000)
-                redis.publish("candlesocket",frappe.as_json({"scanner":"premarket","title":self.public_name,"data":"%s %s"% (active,val)}))
+    def start(self):        
+        redis = get_redis_server()
+        val = 1
+        while(True):
+            val=val+1 
+            time.sleep(self.update_ms/1000)
+            redis.publish("candlesocket",frappe.as_json({"scanner":"premarket","title":self.public_name,"data":"%s %s"% (active,val)}))
 
