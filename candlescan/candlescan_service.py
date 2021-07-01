06916175fad7b5b5629e7137639492a0ba425b10
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 11:39:43 +0200

    Update candlescan_service.py

--- candlescan/candlescan_service.py
+++ candlescan/candlescan_service.py
@@ -1,6 +1,14 @@
-import frappe
+import frappe, random
 
 
 def after_signup(customer):
     if not customer:
         return
+    if not customer.confirm:
+        customer.confirm = random.randrange(1000,99999)
+    if not customer.referral:
+        customer.referral = frappe.generate_hash(length=10)
+    if not customer.user_key:
+        customer.user_key = frappe.generate_hash(length=15)
+    
+        
