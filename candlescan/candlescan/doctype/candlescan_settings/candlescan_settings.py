8e7ca11bb48bae836ffe4533ef661854fcbcaefd
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 12:22:41 +0200

    Update candlescan_settings.py

--- candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.py
+++ candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe.realtime import get_redis_server
-from frappe.utils.background_jobs import enqueue,get_redis_conn
+from frappe.utils.background_jobs import enqueue,get_redis_conn,get_jobs
 from rq.command import send_stop_job_command
 
 
@@ -17,11 +17,14 @@ class CandlescanSettings(Document):
     def start_scanners(self):
         if self.scanners:
             redis = get_redis_conn()
-            for s in self.scanners:
-                scanner = frappe.get_doc(s.scanner)
-                print(scanner.job_id)
-                if not scanner.job_id:
-                    continue
-                send_stop_job_command(redis, scanner.job_id)
-                if scanner.active:
-                    enqueue(scanner.start, queue='background', job_name=scanner.job_id, job_id=scanner.job_id)
+            jobs = get_jobs(queue="default")
+            for job in jobs:
+                print(job.get_id())
+            #for s in self.scanners:
+            #    scanner = frappe.get_doc(s.scanner)
+            #    print(scanner.job_id)
+            #    if not scanner.job_id:
+            #        continue
+            #    send_stop_job_command(redis, scanner.job_id)
+            #    if scanner.active:
+            #        enqueue(scanner.start, queue='background', job_name=scanner.job_id, job_id=scanner.job_id)
