2dcbdb5649d27b920039546f1edcf37d39da969a
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-01 16:10:46 +0200

    Update premarket_scanner.py

--- candlescan/candlescan/doctype/premarket_scanner/premarket_scanner.py
+++ candlescan/candlescan/doctype/premarket_scanner/premarket_scanner.py
@@ -17,7 +17,7 @@ def start():
     val = 1
     doc = frappe.get_doc("Premarket Scanner")
     while(True):
-        stop = frappe.cache().get_value('stop_%s' % doc.job_id)
+        stop = frappe.cache().get_value('stop_%s' % doc.job_id,shared=True)
         print("stop")
         print(stop)
         if stop == 1:
