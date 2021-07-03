0a7a0e13d08531da46d7d33f6aa499111103adc1
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-03 19:06:28 -0400

    Update candlescan_settings.js

--- candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.js
+++ candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.js
@@ -6,7 +6,7 @@ frappe.ui.form.on('Candlescan Settings', {
 		frm.add_custom_button("Reload scanners",
 							() => {
 								frappe.call({
-									method: 'candlescan.candlescan.candlescan_api.start_scanners',
+									method: 'candlescan.candlescan.candlescan.candlescan_api.start_scanners',
 									args: { },
 									callback(r) {
 										console.log(r);
