6af79632a0e7597ba9c92732f6097dc7329a94aa
frappe <frappe@ubuntu.vm>
2021-07-04 09:24:29 +0000

    vl

---
+++ candlescan/candlescan/doctype/scanner_volume_leaders/scanner_volume_leaders.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, ovresko and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Scanner Volume Leaders', {
+	// refresh: function(frm) {
+
+	// }
+});
