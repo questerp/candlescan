32039bc98d7131661f244f203bc7e0c595ac942f
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-04 08:01:41 -0400

    Update candlescan_service.py

--- candlescan/candlescan_service.py
+++ candlescan/candlescan_service.py
@@ -4,6 +4,7 @@ from frappe.model.document import Document
 from frappe.realtime import get_redis_server
 from frappe.utils.background_jobs import enqueue,get_redis_conn,get_jobs,enqueue_doc,execute_job
 from rq.job import Job
+from frappe.utils import cstr
 from rq.registry import StartedJobRegistry
 from rq import Connection, Queue, Worker
 
