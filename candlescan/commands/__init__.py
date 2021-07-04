f3e2d73a6f0ba7063cccb930121845c095699e4a
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-04 07:46:53 -0400

    Update __init__.py

--- candlescan/commands/__init__.py
+++ candlescan/commands/__init__.py
@@ -3,16 +3,16 @@ from __future__ import unicode_literals, absolute_import, print_function
 import click
 import frappe
 from frappe.commands import pass_context, get_site
-from candlescan.candlescan_service import start_workers
 
 def call_command(cmd, context):
 	return click.Context(cmd, obj=context).forward(cmd)
 
 @click.command('start-candlescan-workers')
-def start_workers():
+def start_candlescan_workers():
 	print("Starting Candlescan Workers")
+	from candlescan.candlescan_service import start_workers
 	start_workers()
 
 commands = [
-	start_workers
+	start_candlescan_workers
 ]
