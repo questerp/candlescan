0b0964114b11485b897ba4c98224501779e704f9
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-06 08:34:10 -0400

    Update scanner_volume_leaders.py

--- candlescan/candlescan/doctype/scanner_volume_leaders/scanner_volume_leaders.py
+++ candlescan/candlescan/doctype/scanner_volume_leaders/scanner_volume_leaders.py
@@ -45,4 +45,4 @@ def start(scanner_id):
 			resultdata.append(  {"symbol":i.name,"volume":rprice})
 
 		redis.publish("candlesocket",frappe.as_json({"scanner_id":scanner_id,"data":resultdata}))
-		redis.publish("candlesocket",frappe.as_json({"scanner_id":"alerts","data":{"symbol":"AAPL","price":152}}))
+		#redis.publish("candlesocket",frappe.as_json({"scanner_id":"alerts","data":{"symbol":"AAPL","price":152}}))
