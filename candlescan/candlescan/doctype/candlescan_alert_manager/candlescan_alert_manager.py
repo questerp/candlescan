c78b85993f34b4291f80b713af0c7749861d12a3
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-06 12:28:30 -0400

    Update candlescan_alert_manager.py

--- candlescan/candlescan/doctype/candlescan_alert_manager/candlescan_alert_manager.py
+++ candlescan/candlescan/doctype/candlescan_alert_manager/candlescan_alert_manager.py
@@ -3,8 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.realtime import get_redis_server
 
 class CandlescanAlertManager(Document):
 	pass
+
+
+@frappe.whitelist()        
+def proccess_alerts:
+	redis = get_redis_server()
+	alerts = frappe.db.sql(""" select user,symbol,filters_script from `tabPrice Alert` where enabled=1 and triggered=0 """,as_dict=True)
+	if not alerts:
+		return
+	for alert in alerts:
+		price = frappe.db.get_value("Symbol",alert.symbol,"price")
+		socket_id = frappe.db.get_value("Customer",alert.user,"socket_id")
+		# decode filter script here (filter_script -> sql condition)
+		if price and socket_id:
+			redis.publish("candlescan_single",frappe.as_json({"socket_id":socket_id,"msg":'%s price is above $_X_' % alert.symbol}))
+		
