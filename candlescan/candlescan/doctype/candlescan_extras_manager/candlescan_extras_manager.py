8856da7934e8813910093983b2a0f25c9c22040a
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-07 10:54:34 -0400

    Update candlescan_extras_manager.py

--- candlescan/candlescan/doctype/candlescan_extras_manager/candlescan_extras_manager.py
+++ candlescan/candlescan/doctype/candlescan_extras_manager/candlescan_extras_manager.py
@@ -19,7 +19,7 @@ def process_extras():
 	pages = int(count / batch_size) + 1
 	for page in range(0,pages,1):
 		offset = offset * page
-		symbols = frappe.db.sql("""SELECT name FROM `tabSymbol` OFFSET %s FETCH NEXT %s ROWS ONLY;  """ % (offset,batch_size),as_dict=True)
+		symbols = frappe.db.sql("""SELECT name FROM `tabSymbol` LIMIT %s OFFSET %s """ % (batch_size,offset),as_dict=True)
 		for symbol in symbols:
 			volume = random.randrange(2000000, 5000000)
 			frappe.db.set_value("Symbol",symbol.name,"volume",volume)
