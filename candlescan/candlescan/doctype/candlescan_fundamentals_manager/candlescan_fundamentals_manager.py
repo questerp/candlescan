3de70e9acc02956d4703c3b0a50f2f8baa80b545
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-09 07:37:59 -0400

    Update candlescan_fundamentals_manager.py

--- candlescan/candlescan/doctype/candlescan_fundamentals_manager/candlescan_fundamentals_manager.py
+++ candlescan/candlescan/doctype/candlescan_fundamentals_manager/candlescan_fundamentals_manager.py
@@ -28,12 +28,36 @@ def process():
 	settings.save()
 	yf = YF([a['name'] for a in symbols])
 	data = yf.get_key_statistics_data()
+	summaries = yf.get_stock_data("profile","assetProfile")
+	prices = yf.get_stock_price_data()
 	for s in symbols:
 		stats = data[s.name]
+		summary = summaries[s.name]
+		price = prices[s.name]
+		if price:
+			clean_price =  cstr(json.dumps(price))
+			short_name = price['shortName']
+			exchange = price['exchangeName']
+			frappe.db.set_value("Symbol",s.name,"key_price_data",clean_price,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"company",short_name,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"exchange",exchange,update_modified=False)
+			
 		if stats:
 			clean =  cstr(json.dumps(stats))
 			#print(clean)
-			frappe.db.set_value("Symbol",s.name,"key_statistics_data",clean)
+			frappe.db.set_value("Symbol",s.name,"key_statistics_data",clean,update_modified=False)
+			
+		if summary:
+			website = summary['website']
+			industry = summary['industry']
+			sector = summary['sector']
+			company_summary = summary['longBusinessSummary']
+			clean_summary =  cstr(json.dumps(summary))
+			frappe.db.set_value("Symbol",s.name,"key_summary_data",clean_summary,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"summary",company_summary,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"sector",sector,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"industry_type",industry,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"website",website,update_modified=False)
 		
 	frappe.db.commit()
 	
