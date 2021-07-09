a1faca33ef70cad6b79c809065806aded58d6292
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-09 13:53:27 -0400

    Update get_tickers.py

--- candlescan/get_tickers.py
+++ candlescan/get_tickers.py
@@ -222,7 +222,7 @@ def __exchange2list(exchange,regions=None,sectors=None,countries=None,analystRat
     #df_filtered = df[~df['symbol'].str.contains("\.|\^")]
     if df.empty:
         return []
-    return df['symbol'].tolist()
+    return df['symbol','name'].tolist()
 
 
 # save the tickers to a CSV
