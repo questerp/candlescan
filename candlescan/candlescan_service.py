5d167981bfff7581f5249e2b0b9a588184b9fed7
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-11 06:24:06 -0400

    Update candlescan_service.py

--- candlescan/candlescan_service.py
+++ candlescan/candlescan_service.py
@@ -12,13 +12,14 @@ from candlescan import handle
 
 def get_last_broadcast(doctype,scanner_id):
     if not (doctype or scanner_id):
-        return
+        return handle(False,'No scanner_id')
     raw_state = frappe.db.get_value(doctype,"state")
     if raw_state:
         data = json.loads(raw_state)
         parsed_data = frappe.as_json({"scanner_id":scanner_id,"data":data})
-        return handle(True,'Success',parsed_data)
-    return handle(True,'Success')
+        if data and parsed_data:
+            return handle(True,'Success',parsed_data)
+    return handle(True,'No history')
 
 def broadcast(doctype,scanner_id,interval,data):
     redis = get_redis_server()
