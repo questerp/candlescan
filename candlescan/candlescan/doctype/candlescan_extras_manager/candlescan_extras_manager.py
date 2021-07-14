26e9d4c8a31ad265889f166aca2cfddc12f40639
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-14 16:13:52 -0400

    Update candlescan_extras_manager.py

--- candlescan/candlescan/doctype/candlescan_extras_manager/candlescan_extras_manager.py
+++ candlescan/candlescan/doctype/candlescan_extras_manager/candlescan_extras_manager.py
@@ -13,23 +13,4 @@ class CandlescanExtrasManager(Document):
 
 @frappe.whitelist()
 def process_extras():
-	extras_manager = frappe.get_doc("Candlescan Extras Manager")
-	batch_size = extras_manager.batch_size
-	offset = 0
-	count = frappe.db.count("Symbol")
-	pages = int(count / batch_size) + 1
-	while(True):
-		extras_manager  = frappe.get_doc("Candlescan Extras Manager")
-		if not extras_manager.activate:
-			break;
-		time.sleep(5)
-		for page in range(0,pages,1):
-			offset = offset * page
-			symbols = frappe.db.sql("""SELECT name FROM `tabSymbol` LIMIT %s OFFSET %s """ % (batch_size,offset),as_dict=True)
-			for symbol in symbols:
-				volume = random.randrange(2000000, 5000000)
-				frappe.db.set_value("Symbol",symbol.name,"volume",volume)
-
-			frappe.db.commit()
-		
-		# API CALL to get data
+	pass
