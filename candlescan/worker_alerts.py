936595ec0533e048b600e31fadbedd2ee830c3f0
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-14 18:57:37 -0400

    Update worker_alerts.py

--- candlescan/worker_alerts.py
+++ candlescan/worker_alerts.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, ovresko and contributors
+# For license information, please see license.txt
 
+from __future__ import unicode_literals
+import frappe
+from frappe.realtime import get_redis_server
+
+def process():
+	redis = get_redis_server()
+	alerts = frappe.db.sql(""" select user,symbol,filters_script from `tabPrice Alert` where enabled=1 and triggered=0 """,as_dict=True)
+	if not alerts:
+		return
+	for alert in alerts:
+		print("processing alert")
+		print(alert)
+		price = frappe.db.get_value("Symbol",alert.symbol,"price")
+		socket_id = frappe.db.get_value("Customer",alert.user,"socket_id")
+		# decode filter script here (filter_script -> sql condition)
+		if price and socket_id:
+			print(price,socket_id)
+			redis.publish("candlescan_single",frappe.as_json({"socket_id":socket_id,"data":'%s price is above $_X_' % alert.symbol}))
