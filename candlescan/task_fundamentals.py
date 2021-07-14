ebd58f9094eca497a7cb7a37724938102fd8c4d4
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-14 18:50:12 -0400

    Update task_fundamentals.py

--- candlescan/task_fundamentals.py
+++ candlescan/task_fundamentals.py
@@ -1,1 +1,67 @@
+from __future__ import unicode_literals
+from candlescan.candlescan_yf import YahooFinancials as YF
+import frappe, json
+from frappe.utils import cstr
+import requests
 
+def process():
+	settings =frappe.get_doc("Fundamentals")
+	activate = settings.activate
+	if not activate:
+		return
+	batch = settings.batch
+	offset =  settings.offset
+	count = frappe.db.count("Symbol")
+	settings.offset = offset + batch
+	if offset >= count:
+		settings.offset = 0
+		offset = 0
+	symbols = frappe.db.sql(""" select name,exchange from `tabSymbol` LIMIT %s OFFSET %s """ % (batch,offset),as_dict=True)
+	settings.save()
+	global yf
+	if batch == 1 and len(symbols) == 1:
+		_symbol = symbols[0]
+		yf = YF(_symbol.name)
+	else:
+		yf = YF([a['name'] for a in symbols])
+	data = yf.get_key_statistics_data()
+	summaries = yf.get_stock_profile_data()
+	prices = yf.get_stock_price_data()
+	sumdatas = yf.get_stock_summary_detail()
+	for s in symbols:
+		print("Fetching %s" % s.name)
+		stats = data[s.name] if s.name in data else []
+		summary = summaries[s.name] if s.name in summaries else []
+		price = prices[s.name] if s.name in prices else []
+		sumdata = sumdatas[s.name] if s.name in sumdatas else []
+		if sumdata:
+			clean_sumdata =  cstr(json.dumps(sumdata))
+			frappe.db.set_value("Symbol",s.name,"stock_summary_detail",clean_sumdata,update_modified=False)
+			
+		if price:
+			clean_price =  cstr(json.dumps(price))
+			short_name = price['shortName'] if 'shortName' in price else ''
+			exchange = price['exchangeName'] if 'exchangeName' in price else ''
+			frappe.db.set_value("Symbol",s.name,"key_price_data",clean_price,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"company",short_name,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"exchange",exchange,update_modified=False)
+			
+		if stats:
+			clean =  cstr(json.dumps(stats))
+			#print(clean)
+			frappe.db.set_value("Symbol",s.name,"key_statistics_data",clean,update_modified=False)
+			
+		if summary:
+			website =  summary['website'] if 'website' in summary else ''
+			industry = summary['industry'] if 'industry' in summary else ''
+			sector = summary['sector']  if 'sector' in summary else ''
+			company_summary = summary['longBusinessSummary']  if 'longBusinessSummary' in summary else ''
+			clean_summary =  cstr(json.dumps(summary))
+			frappe.db.set_value("Symbol",s.name,"key_summary_data",clean_summary,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"summary",company_summary,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"sector",sector,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"industry_type",industry,update_modified=False)
+			frappe.db.set_value("Symbol",s.name,"website",website,update_modified=False)
+		
+	frappe.db.commit()
+	
