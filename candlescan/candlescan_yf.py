743cebb39ab9efb0db56b1ec65702a7b1bf99f99
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-14 18:44:18 -0400

    Update candlescan_yf.py

--- candlescan/candlescan_yf.py
+++ candlescan/candlescan_yf.py
@@ -16,58 +16,6 @@ import frappe
 # track the last get timestamp to add a minimum delay between gets - be nice!
 _lastget = 0
 
-def fetch_calendars():
-    targets = ["earnings","splits","ipo","economic"]
-    for target in targets:
-        url = 'https://finance.yahoo.com/calendar/'+target
-        urlopener = UrlOpener()
-        # Try to open the URL up to 10 times sleeping random time if something goes wrong
-        max_retry = 10
-        data = None
-        for i in range(0, max_retry):
-           response = urlopener.open(url)
-           if response.getcode() != 200:
-               time.sleep(random.randrange(10, 20))
-           else:
-               response_content = response.read()
-               soup = BeautifulSoup(response_content, "html.parser")
-               re_script = soup.find("script", text=re.compile("root.App.main"))
-               if re_script is not None:
-                   script = re_script.text
-                   # bs4 4.9.0 changed so text from scripts is no longer considered text
-                   if not script:
-                       script = re_script.string
-                   data = loads(re.search("root.App.main\s+=\s+(\{.*\})", script).group(1))
-                   response.close()
-                   break
-               else:
-                   time.sleep(random.randrange(10, 20))
-           if i == max_retry - 1:
-               break
-
-        if data:
-            rows = data["context"]["dispatcher"]["stores"]["ScreenerResultsStore"]["results"]["rows"]
-            if rows:
-                #print(rows)
-                for row in rows:
-                    #print(row)
-                    if 'ticker' in row:
-                        ticker = row['ticker']
-                        if ticker and not frappe.db.exists("Symbol",ticker):
-                            companyshortname = row['companyshortname'] if 'companyshortname' in row else ''
-                            exchange_short_name= row['exchange_short_name'] if 'exchange_short_name' in row else 'N/A'
-                            print(ticker)
-                            new_symbol = frappe.get_doc({
-                                'doctype':'Symbol',
-                                'symbol':ticker,
-                                'company':companyshortname,
-                                'exchange':exchange_short_name
-                            })
-                            new_symbol.insert()
-                json_rows = dumps(rows)
-                frappe.db.set_value("Candlescan Fundamentals Manager",None,target,json_rows)
-                time.sleep(3)
-    frappe.db.commit()
 
 # Custom Exception class to handle custom error
 class ManagedException(Exception):
