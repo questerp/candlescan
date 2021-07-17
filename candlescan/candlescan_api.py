045961c196351b29c5f757033440d537eb283420
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-17 18:51:32 -0400

    Update candlescan_api.py

--- candlescan/candlescan_api.py
+++ candlescan/candlescan_api.py
@@ -106,11 +106,11 @@ def run_stock_filter(user,name):
     if not (user or name):
         frappe.throw("Missing data")
     filter = frappe.get_doc("Stock Filter",name)
-    if filter.script:
-        sql = json.loads(filter.script)
+    if filter.sql_script:
+        sql = json.loads(filter.sql_script)
         sort = "ASC" if filter.sort_mode == "Ascending" else "DESC"
         if sql:
-            data = frappe.db.sql(""" select symbol from tabSymbol where %s order by %s %s limit 100""" % (sql,sort,filter.sort_field),as_dict=True)
+            data = frappe.db.sql(""" select symbol from tabSymbol where %s order by %s %s limit 100""" % (sql,filter.sort_field,sort),as_dict=True)
             return handle(True,"Success",data)
     frappe.throw("Can't execute filter, contact support")
     
