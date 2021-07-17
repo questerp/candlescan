513d17025d75990415cd26500279b632be2fe095
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-17 11:46:26 -0400

    Update stock_filter.py

--- candlescan/candlescan/doctype/stock_filter/stock_filter.py
+++ candlescan/candlescan/doctype/stock_filter/stock_filter.py
@@ -5,6 +5,34 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+import sqlvalidator
 
 class StockFilter(Document):
-	pass
+	def validate(self):
+		sql = self.validate_script()
+		sql_query = sqlvalidator.parse(sql)
+		if not sql_query.is_valid():
+		    frappe.throw(sql_query.errors)
+		
+	def validate_script(self):
+		if not self.script:
+			frappe.throw("Script is required")
+		
+		or_blocks = self.script.split(' OR ')
+		sql =""
+		and_sqls = []
+		for or_block in or_blocks:
+			and_blocks = or_block.splitlines()
+			and_sql = " AND ".join(and_blocks)
+			and_sql = "( %s )" % and_sql
+			and_sqls.append(and_sql)
+			
+		if and_sqls:
+			if len(and_sqls) > 1:
+				sql = " OR ".join(and_sqls)
+			else:
+				sql = and_sqls
+				
+		return sql
+		
+		
