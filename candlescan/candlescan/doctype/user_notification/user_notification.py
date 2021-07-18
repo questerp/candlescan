e25b4026ad44d9bc1dcc1c8171982599712fc13c
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-18 14:33:32 -0400

    Update user_notification.py

--- candlescan/candlescan/doctype/user_notification/user_notification.py
+++ candlescan/candlescan/doctype/user_notification/user_notification.py
@@ -7,6 +7,6 @@ import frappe
 from frappe.model.document import Document
 
 class UserNotification(Document):
-	def after_insert(self):
+	def before_insert(self):
 		frappe.db.sql("""DELETE from `tabUser Notification` where user='%s' and message='%s'""" % (self.user,self.message))
 		frappe.db.commit()
