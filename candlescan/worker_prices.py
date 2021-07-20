5452a0f700e367b5c92f4a35cbae8d317174a2e5
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-20 13:47:56 -0400

    Update worker_prices.py

--- candlescan/worker_prices.py
+++ candlescan/worker_prices.py
@@ -1,9 +1,12 @@
 import candlescan.yahoo_finance_api2 as yf
+from frappe.utils import cint
 
 def get_prices(symbol,period_type, period, frequency_type, frequency):
 	if not (symbol or period_type or period or frequency_type or frequency):
 		return
 	
 	share = yf.Share(symbol)
+	period = cint(period)
+	frequency = cint(frequency)
 	data = share.get_historical(period_type,period,frequency_type,frequency,"dict")
 	return data
