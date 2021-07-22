e8f7ffe3302a3b96a258835542546ec10e4ec5b3
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-22 18:14:04 -0400

    Create paltform.py

---
+++ candlescan/paltform.py
@@ -0,0 +1,28 @@
+import frappe
+from candlescan.candlescan_api import handle
+
+def get_platform_data(user):    
+	if not user:
+		return handle(False,"User is required")
+	alerts = frappe.db.sql(""" select name,user,creation, enabled, filters_script, symbol, triggered, notify_by_email from `tabPrice Alert` where user='%s'""" % (user),as_dict=True)
+	extras = frappe.db.get_single_value('Candlescan Settings', 'extras')
+	scanners = frappe.db.sql(""" select default_config,title,description,active,scanner_id,scanner,method from `tabCandlescan scanner` """,as_dict=True)
+	customScanners = frappe.db.sql(""" select title,scanner,name,user,config,target from `tabCustom Scanner` where user='%s' """ % (user),as_dict=True)
+	layouts = frappe.db.sql(""" select title,name,config  from `tabLayout` where user='%s' """ % (user),as_dict=True)
+	watchlists = frappe.db.sql(""" select name,watchlist,symbols from `tabWatchlist` where user='%s' """ % (user),as_dict=True)
+	filters = frappe.db.sql(""" select sound,notify_all,name,filters,refresh,columns,title,script,sort_field,sort_mode from `tabStock Filter` where user='%s' """ % (user),as_dict=True)
+	fExtras = []
+	if extras:
+		extras = extras.splitlines()
+	for ex in extras:
+		name, label, value_type,doctype = ex.split(':')
+		fExtras.append({"field":name,"header":label,"value_type":value_type,"doctype":doctype})
+	for scanner in scanners:
+		signautre_method = "%s.signature" % scanner.method
+		config_method = "%s.get_config" % scanner.method
+		signature = frappe.call(signautre_method, **frappe.form_dict)
+		config = frappe.call(config_method, **frappe.form_dict)
+		scanner['signature'] = signature
+		scanner['config'] = config
+
+	return handle(True,"Success",{"filters":filters,"layouts":layouts,"scanners":scanners,"extras":fExtras,"alerts":alerts,"customScanners":customScanners,"watchlists":watchlists})
