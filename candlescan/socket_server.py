4c2f7c78a4e556d3195671aef97ae67d86eda0e8
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-22 14:55:01 -0400

    Update socket_server.py

--- candlescan/socket_server.py
+++ candlescan/socket_server.py
@@ -1,11 +1,13 @@
 import socketio
+import uvicorn
+import asyncio
 
 
 redis_server = None
 redis_addr = "redis://localhost:12000"
 mgr = socketio.RedisManager(redis_addr)
-sio = socketio.Server(async_mode='threading',client_manager=mgr)
-app = socketio.WSGIApp(sio)
+sio = socketio.Server(async_mode='asgi',client_manager=mgr)
+app = socketio.ASGIApp(sio)
 
 def get_redis_server():
 	"""returns redis_socketio connection."""
@@ -29,7 +31,7 @@ def disconnect(sid):
 if __name__ == '__main__':
 	#start_server = websockets.serve(handler,"0.0.0.0",  9002)
 	print("Starting socket at 9002")
-	app.run()
+	uvicorn.run(app, host='0.0.0.0', port=9002)
 
 	#c = get_redis_server()
 	#print(c)
