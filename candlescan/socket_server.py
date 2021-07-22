139327dccf6edfbcec87f3b897df723a200450cd
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-22 12:30:27 -0400

    Update socket_server.py

--- candlescan/socket_server.py
+++ candlescan/socket_server.py
@@ -3,38 +3,30 @@ import json
 import logging
 import websockets
 import queue
+from frappe.realtime import get_redis_server
 
-USERS = set()
+conn = get_redis_server()
 
-
-subscribers = {}
-q_mapping = {}
-register_queue = queue.Queue()
-response_queue = queue.Queue()
-
-async def respond():
-	while 1:
-		try:
-			if response_queue.empty():
-				await asyncio.sleep(0.05)
-				continue
-			response = response_queue.get()
-			print("respond",response)
-			await response["subscriber"].send(response['data'])
-		except:
-			print("error in respond")
+async def respond(user,data):
+	try:
+		if data:
+			socket = conn.hget(user,"socket")
+			if socket:
+				print("socket ",socket)
+				await socket.send(json.dumps(data))
+	except:
+		print("error in respond")
 	
 async def handler(websocket, path):
 	try:
+		if not conn.hexists("sockets",websocket):
+			conn.hset("sockets",websocket,"socket")
 		print("Starting handler")
 		async for msg in websocket:
 			print(msg)
-			if websocket not in subscribers.keys():
-				print("new socket %s" % websocket)
-				subscribers[websocket] = []
-				
 			await websocket.send("Hello!")
-			response_queue.put({"subscriber":websocket,"data":"data from response_queue"})
+			redis.publish("socket",msg)
+			#response_queue.put({"subscriber":websocket,"data":"data from response_queue"})
 				
 	except Exception as e:
 		print(e)
@@ -44,8 +36,5 @@ async def handler(websocket, path):
 if __name__ == '__main__':
 	start_server = websockets.serve(handler,"0.0.0.0",  9002)
 	print("Starting socket at 9002")
-	asyncio.get_event_loop().run_until_complete(asyncio.gather(
-		start_server,
-		respond(),
-		return_exceptions=False))
+	asyncio.get_event_loop().run_until_complete(start_server, return_exceptions=False)
 	asyncio.get_event_loop().run_forever()
