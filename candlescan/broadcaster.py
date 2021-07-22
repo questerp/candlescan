e286e4c5db3e24058418cbf5f89d88601414e13f
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-22 18:45:49 -0400

    Update broadcaster.py

--- candlescan/broadcaster.py
+++ candlescan/broadcaster.py
@@ -3,8 +3,9 @@ from frappe.realtime import get_redis_server
 from candlescan.candlescan_api import handle
 from candlescan.platform import get_platform_data
 from frappe.utils import cstr
+import asyncio
 
-def handle(sid,data):
+async def handle(sid,data):
 	action = data['action']
 	user =  get_redis_server().hget("sockets",sid)
 	if not user:
