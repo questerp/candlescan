70db85f672dfc56b5bcdadeeb06648bb0e6c7a07
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-22 18:14:08 -0400

    Update broadcaster.py

--- candlescan/broadcaster.py
+++ candlescan/broadcaster.py
@@ -1,4 +1,16 @@
 import frappe
+from frappe.realtime import get_redis_server
+from candlescan.candlescan_api import handle
+
+
+def handle(sid,data):
+	action = data['action']
+	user = get_redis_server().hget("sockets",sid)
+	if not user:
+		return handle(False,"Connection lost")
+	# return as handle always
+	if action == "get_platform_data":
+		return candlescan.paltform.get_platform_data(user)
 
 
 def ressource(data):
