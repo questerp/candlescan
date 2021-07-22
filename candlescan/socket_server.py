49c35871965b23f0a4e6fb22cd979e58a95e41e3
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-22 12:45:37 -0400

    Update socket_server.py

--- candlescan/socket_server.py
+++ candlescan/socket_server.py
@@ -4,7 +4,30 @@ import logging
 import websockets
 import queue
 
-	
+
+redis_server = None
+redis_addr = "redis://localhost:12311"
+
+def get_redis_server():
+	"""returns redis_socketio connection."""
+	global redis_server
+	print("get redis")
+	if not redis_server:
+		from redis import Redis
+		redis_server = Redis.from_url(redis_addr)
+	return redis_server
+
+
+async def respond(user,data):
+	try:
+		if data:
+			socket = redis_server.hget(user,"socket")
+			if socket:
+				print("socket ",socket)
+				await socket.send(json.dumps(data))
+	except:
+		print("error in respond")
+		
 async def handler(websocket, path):
 	try:
 		print("Starting handler")
@@ -26,8 +49,8 @@ async def handler(websocket, path):
 if __name__ == '__main__':
 	start_server = websockets.serve(handler,"0.0.0.0",  9002)
 	print("Starting socket at 9002")	
-	#c = get_redis_server()
-	#print(c)
-	asyncio.get_event_loop().run_until_complete(start_server, return_exceptions=False)
+	c = get_redis_server()
+	print(c)
+	asyncio.get_event_loop().run_until_complete(start_server )
 	asyncio.get_event_loop().run_forever()
 
