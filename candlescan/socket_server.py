76cf53043ec06a8ad5580318d6067d35943813d9
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-23 14:36:24 -0400

    Update socket_server.py

--- candlescan/socket_server.py
+++ candlescan/socket_server.py
@@ -74,12 +74,7 @@ def disconnect(sid):
 	get_redis_server().hdel("sockets",user)
 	get_redis_server().hdel("sockets",sid)
 		
-def run():
-	try:
-        # make sure we have an event loop, if not create a new one
-		asyncio.get_event_loop()	
-	except RuntimeError:
-		asyncio.set_event_loop(asyncio.new_event_loop())
+async def run():
 	print("Starting socket at 9002")
 	web.run_app(app, port=9002)
 	
