e974add17dcfd2199d82f0a532d0414c43037964
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-23 12:18:03 -0400

    Update broadcaster.py

--- candlescan/broadcaster.py
+++ candlescan/broadcaster.py
@@ -14,27 +14,8 @@ async def run():
 	await sio.wait()
 	
 @sio.event
-async def from_client(server_sid,data):
-	action = data['action']
-	sid = data['source_sid']
-	user =  get_redis_server().hget("sockets",sid)
-	if not user:
-		return handle(False,"Connection lost")
-	user = cstr(user)
-	# return as handle always
-	
-	if action == "get_platform_data":
-		await sio.emit('to_client',{"client_sid":sid, "data":get_platform_data(user)})
-
-	
-	if action == "get_extras":
-		return handle(True,"Working on it")
-	
-	if action == "subscribe_bars":
-		symbol = data['symbol']
-		timeframe = data['timeframe']
-		get_redis_server().hset("bars",{"sid":sid,"symbol":symbol,"timeframe":timeframe})
-		return handle(True,"Working on it")
+async def from_client(data):
+	pass
 	
 	
 
