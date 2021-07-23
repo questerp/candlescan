809d884c49ef909ee39c3177c873f4ff50445aee
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-23 14:19:27 -0400

    Update broadcaster.py

--- candlescan/broadcaster.py
+++ candlescan/broadcaster.py
@@ -4,26 +4,23 @@ from candlescan.candlescan_api import handle
 from candlescan.platform import get_platform_data
 from frappe.utils import cstr
 import socketio
+import asyncio
 
-sio = socketio.Client(reconnection=True, reconnection_attempts=10, reconnection_delay=1, reconnection_delay_max=5)
+sio = socketio.AsyncClient(reconnection=True, reconnection_attempts=10, reconnection_delay=1, reconnection_delay_max=5)
 
-		
 def run():
+	 asyncio.get_event_loop().run_until_complete(_run())
+
+async def _run():
 	try:
-		sio.connect('http://localhost:9002',auth={"microservice":"broadcaster"})
-		sio.emit("join", "broadcaster")
-		sio.wait()
+		await sio.connect('http://localhost:9002',auth={"microservice":"broadcaster"})
+		await sio.emit("join", "broadcaster")
+		await sio.wait()
 	except socketio.exceptions.ConnectionError as err:
-		sio.sleep(5)
+		await sio.sleep(5)
 		run()
 
-	
+
 @sio.event
-def from_client(data):
+async def from_client(data):
 	pass
-	
-	
-
-def broadcast(sid,data):
-	pass
-
