111526a08a4739354354d32ee20fd79953d15f88
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-23 08:23:21 -0400

    Update broadcaster.py

--- candlescan/broadcaster.py
+++ candlescan/broadcaster.py
@@ -39,4 +39,4 @@ async def from_client(server_sid,data):
 def broadcast(sid,data):
 	pass
 
-asyncio.get_event_loop().run_until_complete(run)
+asyncio.get_event_loop().run_until_complete(run(),return_exceptions=True)
