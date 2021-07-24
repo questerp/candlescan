0f38c9bd7df203828239ddbc59740523981d6227
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-24 17:07:08 -0400

    Update platform.py

--- candlescan/platform.py
+++ candlescan/platform.py
@@ -63,7 +63,7 @@ async def ressource(data):
 			       
 @sio.event
 async def get_platform_data(data):
-	source = data['from']
+	source = data['source_sid']
 	user = get_redis_server().hget("sockets",source)
 	if source and not user:
 		await sio.emit("send_to_client",{"event":"get_platform_data","to":source,"data":"Not connected"})
