579fe9041044221636c85d256be6a114d88512bb
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-27 06:54:49 -0400

    Update data_service.py

--- candlescan/services/data_service.py
+++ candlescan/services/data_service.py
@@ -10,10 +10,13 @@ public_ressources = ["Scanner"]
 
 sio = socketio.AsyncClient(logger=True,json=json_encoder, engineio_logger=True,reconnection=True, reconnection_attempts=10, reconnection_delay=1, reconnection_delay_max=5)
 
-		
+def start():
+	asyncio.get_event_loop().run_until_complete(run())
+	asyncio.get_event_loop().run_forever()
+
 async def run():
 	try:
-		await sio.connect('http://localhost:9002',headers={"microservice":"platform"})
+		await sio.connect('http://localhost:9002',headers={"microservice":"data_service"})
 		await sio.wait()
 	except socketio.exceptions.ConnectionError as err:
 		print("error",sio.sid,err)
