e6fb007a8dfebd1bdf717d75e02500590dbf1130
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-27 06:24:28 -0400

    Create candlescan.py

---
+++ candlescan/utils/candlescan.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+import frappe, random, json
+from frappe.model.document import Document
+from frappe.realtime import get_redis_server
+from frappe.utils.background_jobs import enqueue,get_redis_conn,get_jobs,enqueue_doc,execute_job
+from rq.job import Job
+import time
+from frappe.utils import cstr
+from rq.registry import StartedJobRegistry
+from rq import Connection, Queue, Worker
+from candlescan import handle
+import asyncio
+import threading
+
+
+def clear_user_notifications():
+    frappe.db.sql("""delete from `tabUser Notification` where user IS NOT NULL """)
+    frappe.db.commit()
+
+
+
+    
+def insert_symbol(symbol):
+    symbol.flags.ignore_links = True
+    symbol.flags.ignore_validate = True
+    symbol.flags.ignore_permissions  = True
+    symbol.flags.ignore_mandatory = True
+    symbol.insert()
+    frappe.db.commit()
+	
+
+def generate_customer_codes(customer,method):
+    if not customer:
+        return
+    if not customer.confirm:
+        customer.confirm = random.randrange(1000,99999)
+    if not customer.referral:
+        customer.referral = frappe.generate_hash(length=10)
+    if not customer.user_key:
+        customer.user_key = frappe.generate_hash(length=15)
+    
+    customer.save()	
