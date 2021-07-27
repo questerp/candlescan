65a00c6c6f3ee3baa72896ff5503d3e90b6f9719
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-27 06:25:45 -0400

    Update candlescan.py

--- candlescan/utils/candlescan.py
+++ candlescan/utils/candlescan.py
@@ -11,7 +11,8 @@ from rq import Connection, Queue, Worker
 from candlescan import handle
 import asyncio
 import threading
-
+import candlescan.utils.yahoo_finance_api2 as yf
+from frappe.utils import cint
 
 def clear_user_notifications():
     frappe.db.sql("""delete from `tabUser Notification` where user IS NOT NULL """)
@@ -40,3 +41,15 @@ def generate_customer_codes(customer,method):
         customer.user_key = frappe.generate_hash(length=15)
     
     customer.save()	
+
+	
+def get_yahoo_prices(symbol,period_type, period, frequency_type, frequency):
+	if not (symbol or period_type or period or frequency_type or frequency):
+		return
+	
+	share = yf.Share(symbol)
+	period = cint(period)
+	frequency = cint(frequency)
+	data = share.get_historical(period_type,period,frequency_type,frequency,"dict")
+	data = [a for a in data if a['low'] and a['low']>0]
+	return data
