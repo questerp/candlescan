5a44d13aa06e99517832dfd575184c0295c36397
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-27 06:26:41 -0400

    Update alerts_service.py

--- candlescan/services/alerts_service.py
+++ candlescan/services/alerts_service.py
@@ -1,1 +1,73 @@
 
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, ovresko and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe, json
+from frappe.realtime import get_redis_server
+import time
+from frappe.cache_manager import clear_doctype_cache
+
+
+def process():
+	redis = get_redis_server()
+	while(True):
+		#clear_doctype_cache("Price Alert")
+		time.sleep(5)
+		frappe.local.db.commit()
+		alerts = frappe.db.sql(""" select name,user,symbol,filters_script,notify_by_email,enabled,triggered from `tabPrice Alert` where enabled=1 and triggered=0 limit 100""",as_dict=True)
+		if not alerts:
+			print("No alerts")
+			continue
+		for alert in alerts:
+			if not alert.filters_script:
+				continue
+			filters_script = alert.filters_script
+			filters = json.loads(filters_script)
+			sql_filter = convert_filters_script(filters)
+			symbol = alert.symbol
+			if sql_filter and symbol:
+				scr = """ select name from tabSymbol where symbol = '{symbol}' and {filter}  """.format(symbol=symbol,filter=sql_filter)
+				print("scr %s" % scr)
+				exists = frappe.db.sql(scr,as_dict=True)
+				print("exists %s" % exists)
+				if exists:
+					socket_id = frappe.db.get_value("Customer",alert.user,"socket_id")
+					print("socket_id %s" % socket_id)
+					if socket_id:
+						doc = frappe.db.set_value("Price Alert",alert.name,"triggered",1)
+						#doc.triggered = True
+						#doc.save()
+						frappe.db.commit()
+						session = frappe.db.sql(""" select token from `tabWeb Session` where user='%s'""" % alert.user,as_dict=True)
+						if session:
+							redis.publish("candlescan_single",frappe.as_json({"socket_id":socket_id,"data":'%s alert is triggered' % alert.symbol}))
+		
+
+def convert_filters_script(filters):
+	if not filters:
+		return ''
+	sql = ""
+	cond = []
+	for filter in filters:
+		operator = convert_operator(filter['operator'])
+		field = filter['column']['field']
+		value = filter['value']
+		value_max = filter['value_max']
+		if operator and value and operator != 'BETWEEN':
+			sc = "%s %s %s" % (field,operator,value)
+			cond.append(sc)
+		elif operator and value and operator == 'BETWEEN' and value_max:
+			sc = "%s %s %s AND %s" % (field,operator,value,value_max)
+			cond.append(sc)
+	if cond:
+		sql = " and ".join(cond)
+	print("sql %s" % sql)
+	return sql
+		
+def convert_operator(operator):
+	if not operator:
+		return ""
+	return ">" if operator == "Above" else "<" if operator == "Below" else "BETWEEN" if operator == "Between" else ""
+		
