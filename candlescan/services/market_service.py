b4b50d275daa594536dad50931a0b8649b073786
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-29 19:46:06 -0400

    Update market_service.py

--- candlescan/services/market_service.py
+++ candlescan/services/market_service.py
@@ -33,6 +33,10 @@ async def run():
 			await asyncio.sleep(3)
 			price = random.uniform(1,10)
 			await sio.emit("transfer",build_response("price","MSON",{"symbol":"MSON","price":price}))
+			await sio.emit("transfer",build_response("price","NTEC",{"symbol":"NTEC","price":price}))
+			await sio.emit("transfer",build_response("price","MDIA",{"symbol":"MDIA","price":price}))
+			await sio.emit("transfer",build_response("price","SGLB",{"symbol":"SGLB","price":price}))
+			await sio.emit("transfer",build_response("price","SGOC",{"symbol":"SGOC","price":price}))
 		
 	except socketio.exceptions.ConnectionError as err:
 		print("error",sio.sid,err)
