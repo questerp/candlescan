15b1e269795a0f58b42615fc99ea15c1e3b9e2c8
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-29 14:51:46 -0400

    Update market_service.py

--- candlescan/services/market_service.py
+++ candlescan/services/market_service.py
@@ -17,6 +17,34 @@ import datetime
 import pytz
 import random
 
+
+
+sio = socketio.AsyncClient(logger=True,json=json_encoder, engineio_logger=True,reconnection=True, reconnection_attempts=10, reconnection_delay=1, reconnection_delay_max=5)
+
+def start():
+	asyncio.get_event_loop().run_until_complete(run())
+	asyncio.get_event_loop().run_forever()
+
+async def run():
+	try:
+		await sio.connect('http://localhost:9002',headers={"microservice":"market_service"})
+		await sio.wait()
+	except socketio.exceptions.ConnectionError as err:
+		print("error",sio.sid,err)
+		await sio.sleep(5)
+		await run()
+
+def init():
+	frappe.connect()	
+
+@sio.event
+async def connect():
+	init()
+	print("I'm connected!")
+
+
+
+
 try:
     from urllib import FancyURLopener
 except:
