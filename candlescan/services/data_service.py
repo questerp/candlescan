3448de3a013506916df691acd33224d30a00a292
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-30 05:45:31 -0400

    Update data_service.py

--- candlescan/services/data_service.py
+++ candlescan/services/data_service.py
@@ -53,10 +53,9 @@ async def get_last_result(message):
 	if not scanner_id:
 		return
 	
-	results = frappe.db.sql("""select state from `tabScanner Result` where scanner='%s' order by date desc limit 1""" % scanner_id,as_dict=True)
-	if results:
-		data = json.loads(results[0].state)
-		await sio.emit("transfer",build_response("get_last_result",source_sid,data))
+	results = frappe.db.sql("""select state,date from `tabScanner Result` where scanner='%s' order by date desc limit 1""" % scanner_id,as_dict=True)
+	if results and len(results):
+		await sio.emit("transfer",build_response("get_last_result",source_sid,results[0]))
 		
 	
 	
