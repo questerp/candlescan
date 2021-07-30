16b55959b4f81cb182793915ae1638b00b568ff8
ovresko <32199620+ovresko@users.noreply.github.com>
2021-07-30 11:04:01 -0400

    Update data_service.py

--- candlescan/services/data_service.py
+++ candlescan/services/data_service.py
@@ -124,7 +124,7 @@ async def ressource(message):
 				#await sio.emit("send_to_client",build_response("ressource",source_sid,response))
 
 	if method == "delete" and name:
-		frappe.delete_doc(doctype, name, ignore_missing=True,delete_permanently=True)
+		frappe.delete_doc(doctype, name, ignore_missing=True)
 		frappe.db.commit()
 		await sio.emit("transfer",build_response("ressource",source_sid,{"method":method,"doctype":doctype,"data":"Deleted"}))
 		#await sio.emit("send_to_client",build_response("ressource",source_sid,"Deleted"))
