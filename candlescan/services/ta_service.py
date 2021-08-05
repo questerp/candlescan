dd24c91c099efaa9b78d1a04dd95d231ef16ec26
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-05 15:58:00 -0400

    Update ta_service.py

--- candlescan/services/ta_service.py
+++ candlescan/services/ta_service.py
@@ -20,7 +20,7 @@ async def run():
 		await sio.connect('http://localhost:9002',headers={"microservice":"ta_service"})
 		stream = Stream()  # <- replace to SIP if you have PRO subscription
 		stream.subscribe_bars(handle_subs,"*")
-		stream.run()
+		await stream._run_forever()
 		run_connection(stream)
 		await sio.wait()
 	except socketio.exceptions.ConnectionError as err:
