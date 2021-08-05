11753cab5d89fd272feebbc17dd9917ff4ece04e
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-05 15:37:48 -0400

    Update ta_service.py

--- candlescan/services/ta_service.py
+++ candlescan/services/ta_service.py
@@ -10,8 +10,7 @@ from alpaca_trade_api.rest import REST
 
 
 sio = socketio.AsyncClient(logger=True,json=json_encoder, engineio_logger=True,reconnection=True, reconnection_attempts=10, reconnection_delay=1, reconnection_delay_max=5)
-api = REST(raw_data=True)
-
+stream = None
 def start():
 	asyncio.get_event_loop().run_until_complete(run())
 	asyncio.get_event_loop().run_forever()
@@ -19,39 +18,35 @@ def start():
 async def run():
 	try:
 		await sio.connect('http://localhost:9002',headers={"microservice":"ta_service"})
-		while(1):
-			handle_subs()
-			await sio.sleep(5)
+		stream = Stream()  # <- replace to SIP if you have PRO subscription
+		stream.subscribe_bars(handle_subs,["*"])
+
+		await sio.await()
 	except socketio.exceptions.ConnectionError as err:
 		print("error",sio.sid,err)
 		await sio.sleep(5)
 		await run()
 
-def handle_subs():
-	_symbols = get_redis_server().smembers("symbols")
-	symbols = [cstr(a) for a in _symbols]
-	print(_symbols)
-	if symbols:
-		snap = api.get_snapshots(symbols)
-		for st in snap:
-			print(st)
-			s = snap[st]
-			if s:
-				latest_trade = s.get("latestTrade")
-				latest_quote = s.get("latestQuote")
-				minute_bar =  s.get("minuteBar")
-				daily_bar =  s.get("dailyBar")
-				prev_daily_bar =  s.get("prevDailyBar")
-
-				if latest_trade:
-					price = latest_trade.get("p") if latest_trade else 0
-					volume = daily_bar.get("v") if daily_bar else 0
-					bid = latest_quote.get("bp") if latest_quote else 0
-					ask = latest_quote.get("ap") if latest_quote else 0
-					print(s,price,volume,bid,ask)
-					
-					frappe.db.sql(""" update tabSymbol set price=%s, volume=%s, bid=%s, ask=%s where symbol='%s'""" % (price,volume,bid,ask,st))
-		frappe.db.commit()
+async def handle_subs(data):
+	if data:
+		price = data[0]
+		price['doctype'] = "Bars"
+		print(price)
+		frappe.get_doc(price).insert(ignore_permissions=True, ignore_if_duplicate=True,	ignore_mandatory=True,  set_child_names=False)
+	#[
+	#{
+	#"T": "b",
+	#"S": "AMC",
+	#"o": 33.44,
+	#"c": 33.35,
+	#"h": 33.52,
+	#""l": 33.35,
+	#""v": 1407,
+	#""t": "2021-08-05T19:32:00Z",
+	#"n": 18,
+	#""vw": 33.419701
+	#"}
+	#]
 			
 		
 
