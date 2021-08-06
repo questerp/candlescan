786fee4f5f53d8c218079025bcd2eb5cb5adc703
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-06 12:49:30 -0400

    Update price_service.py

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -7,47 +7,29 @@ from frappe.realtime import get_redis_server
 from candlescan.utils.socket_utils import get_user,validate_data,build_response,json_encoder
 from alpaca_trade_api import Stream
 from alpaca_trade_api.common import URL
+from alpaca_trade_api.rest import REST
+
 
 log = logging.getLogger(__name__)
+api = None
 
 def start():
 	logging.basicConfig(level=logging.INFO)
-	stream = Stream(base_url=URL('https://paper-api.alpaca.markets'), data_feed='iex', raw_data=True)
-	#stream.run()
-	run_connection(stream)
-		
-def run_connection(conn):
-	try:
-		conn.subscribe_bars(handle_subs,"*")
-		conn.run()
-	except Exception as e:
-		print(f'Exception from websocket connection: {e}')
-	finally:
-		print("Trying to re-establish connection")
-		time.sleep(3)
-		run_connection(conn)
-
-	
-async def handle_subs(price):
-	print(price)
-	if price:
-		#price = price[0]
-		price['t'] = price['t'].seconds * int(1e9) + price['t'].nanoseconds
-		price['doctype'] = "Bars"
-		frappe.get_doc(price).insert(ignore_permissions=True, ignore_if_duplicate=True,	ignore_mandatory=True)
-
-	#[
-	#{
-	#"T": "b",
-	#"S": "AMC",
-	#"o": 33.44,
-	#"c": 33.35,
-	#"h": 33.52,
-	#""l": 33.35,
-	#""v": 1407,
-	#""t": "2021-08-05T19:32:00Z",
-	#"n": 18,
-	#""vw": 33.419701
-	#"}
-	#]
-		
+	api = REST(raw_data=True)
+	redis = get_redis_server()
+	while(1):
+		_symbols = redis.smembers("symbols")
+		symbols = [cstr(a) for a in _symbols if a]
+		snap = api.get_snapshots(symbols)
+		for s in snap:
+			data = snap[s]
+			print(data)
+			minuteBar = data.get("minuteBar")
+			latestTrade = data.get("latestTrade")
+			dailyBar = data.get("dailyBar")
+			minuteBar['doctype'] = "Bars"
+			frappe.get_doc(minuteBar).insert(ignore_permissions=True, ignore_if_duplicate=True, ignore_mandatory=True)
+			if latestTrade:
+				frappe.db.sql(""" update tabSymbol set price=%s, volume=%s where name='%s'""" % (latestTrade.get("p"),dailyBar.get("v"),s))
+		frappe.db.commit()
+		time.sleep(60)
