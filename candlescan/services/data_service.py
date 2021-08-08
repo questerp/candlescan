59aada33ee58d4f8058adcd4ea6b3ea769bd97c9
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-08 14:10:09 -0400

    Update data_service.py

--- candlescan/services/data_service.py
+++ candlescan/services/data_service.py
@@ -18,7 +18,7 @@ def start():
 async def run():
 	try:
 		await sio.connect('http://localhost:9002',headers={"microservice":"data_service"})
-		keep_alive()
+		await keep_alive()
 	except socketio.exceptions.ConnectionError as err:
 		print("error",sio.sid,err)
 		await sio.sleep(5)
