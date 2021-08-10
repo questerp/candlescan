f0e530ada5c878180e4f9df2a1b412cec5c53242
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-10 18:31:17 -0400

    Update price_service.py

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -163,8 +163,9 @@ def start():
 		#	redis.sadd("5m_symbols",s)
 		if minuteBars:
 			#try:
-			frappe.db.sql("""SET @@session.unique_checks = 0; SET @@session.foreign_key_checks = 0;
-			INSERT INTO `tabBars` (name,s,t,o,h,l,c,v,n,vw)
+			frappe.db.sql("""SET @@session.unique_checks = 0""")
+			frappe.db.sql("""SET @@session.foreign_key_checks = 0""")
+			frappe.db.sql("""INSERT INTO `tabBars` (name,s,t,o,h,l,c,v,n,vw)
 			VALUES {values}""".format(values = ", ".join(["('%s_%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (s['s'],s['t'],s['s'],s['t'],s['o'],s['h'],s['l'],s['c'],s['v'],s['n'],s['vw']) for s in minuteBars])))
 			#except Exception as e:
 			#	print(e)
