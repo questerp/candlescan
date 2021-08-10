4730e6597c4f3b9305481fd12b5947aee3400aed
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-10 13:18:10 -0400

    Update filters_service.py

--- candlescan/services/filters_service.py
+++ candlescan/services/filters_service.py
@@ -47,6 +47,7 @@ async def run_stock_filter(message):
 	if not source_sid:
 		return
 	#filter = frappe.get_doc("Stock Filter",name)
+	frappe.db.commit()
 	filter = frappe.db.sql("select sql_script,limit_results,name,sort_field from `tabStock Filter` where name='%s' limit 1" % name,as_dict=True)
 	if filter:
 		filter = filter[0]
