898e12151eec72803af3c5e0f48478cadba363b7
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-14 12:20:26 -0400

    Update price_service.py

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -322,7 +322,7 @@ def get_minute_bars(symbol,start,end=None):
 	if not end:
 		end = dt.now().timestamp()
 	try:
-		data = [ x for x in table.where("""(ticker == %s) & (time>=%s) & (time<=%s)""" % (symbol,start,end) ) ]
+		data = [ x for x in table.where("""(ticker == '%s') & (time>=%s) & (time<=%s)""" % (symbol,start,end) ) ]
 		return data
 	except Exception as ex:
 		print("ERROR",ex)
