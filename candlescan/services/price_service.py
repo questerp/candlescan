b7e2f29c6ad7cbd8d264328c3d8e47a261f9d7bc
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-17 08:53:07 -0400

    Update price_service.py

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -325,8 +325,9 @@ def insert_minute_bars(minuteBars,send=False):
 			symbol['valide'] = symbol['open'] > 0
 			symbol.append()
 			if send and ticker  and  ticker in symbols:
-				print("queue_data",ticker)
 				ev  = "bars_%s"%  ticker.lower()
+				print("queue_data",ev)
+				
 				queue_data(ev,ev,bar)
 				#sio.emit("transfer",build_response(ev,ev,bar))
 	except Exception as e:
