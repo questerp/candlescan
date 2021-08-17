fbf850bb5efa7c904fcd0106116e074d41c6529b
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-17 14:36:10 -0400

    Update price_service.py

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -196,7 +196,7 @@ def _start():
 def backfill():
 	api = REST(raw_data=True)
 	now = dt.now()
-	start = add_days(dt.now()) #-1
+	start = dt.now()# add_days(dt.now()) #-1
 	start = start.replace(second=0).replace(microsecond=0).replace(hour=4)
 	all_symbols = frappe.db.sql("""select symbol from tabSymbol where active=1 """,as_list=True)
 	all_symbols = [a[0] for a in all_symbols] 
