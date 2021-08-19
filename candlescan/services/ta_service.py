0be5038d8ff972f573e868400e451dabf562d18a
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-19 13:39:39 -0400

    Update ta_service.py

--- candlescan/services/ta_service.py
+++ candlescan/services/ta_service.py
@@ -6,7 +6,6 @@ import socketio
 import asyncio
 from frappe.realtime import get_redis_server
 from candlescan.utils.socket_utils import get_user,validate_data,build_response,json_encoder,keep_alive
-from candlescan.services.price_service import synchronized_open_file,synchronized_close_file
 from datetime import timedelta,datetime as dt
 from frappe.utils import cstr,add_days, get_datetime
 import pandas as pd
@@ -45,31 +44,7 @@ async def run():
 
 	
 def ta(volume_min,volume_max):
-	symbols = frappe.db.sql("select symbol from tabSymbol where active=1 and 1m_volume >=%s and 1m_volume<=%s" %( volume_min,volume_max),as_list=1)
-	symbols = [a[0] for a in symbols]
-	file = synchronized_open_file("r")
-	try:
-		table = file.root.bars_group.bars
-		start = dt.now()
-		for symbol in symbols:
-			id = 0
-			candles = []
-			for candle in table.where("ticker==b'%s'" % symbol):
-				id +=1
-				if id >50:
-					break
-				candles.append(candle[:])
-			print("candles",len(candles))
-			
-			
-			ema = ta.stream(candles)
-			
-			#data = [ x[:] for x in table.where("""(ticker == b'%s') & (time>=%s) & (time<=%s) & (valide)""" % (symbol,start,end) ) ]
-	except Exception as ex:
-		print("ERROR ta",ex)
-		return ex
-	finally:
-		synchronized_close_file(file)
+	pass
 		
 		
 
