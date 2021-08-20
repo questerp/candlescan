7638b1e9b9283e3f237a00398c34a70de38e8ff0
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-20 12:16:51 -0400

    Update price_service.py

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -206,7 +206,7 @@ def init_bars_db():
 		collection = store.collection("1MIN",overwrite=True)
 		symbols = frappe.db.sql("""select symbol from tabSymbol where active=1 """,as_list=True)
 		symbols = [a[0] for a in symbols]
-		df = pd.DataFrame([{"ticker":"","open":0,"close":0,"high":0,"low":0,"volume":0,"trades":0,"time":dt.min}])
+		df = pd.DataFrame([{"ticker":"","open":0,"close":0,"high":0,"low":0,"volume":0,"trades":0,"time":dt.now()}])
 		df.set_index("time",inplace=True)					  
 		
 		for s in symbols:
