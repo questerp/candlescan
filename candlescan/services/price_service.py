e3310d992a6323c1f57d2215306a08d23d9dfb34
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-24 17:19:27 +0100

    q

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -366,7 +366,7 @@ def get_minute_bars(symbol,timeframe,start,end=None):
 	if not end:
 		end = dt.utcnow()#.isoformat()
 	else:
-		end = dt.fromtimestamp(end)
+		end = dt.utcfromtimestamp(end)
 	try:
 		result = []
 		item = None
