393b217c62b0a16cf68a3d96186d35b80fed7949
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-25 23:08:02 +0100

    q

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -116,51 +116,51 @@ def _start():
 				#if minuteBars:
 				insert_minute_bars(s,[minuteBar],True)				
 				price = latestTrade.get("p")
-				if price:
+				#if price:
 					
-					sql = """ update tabSymbol set 
-					price=%s, 
-					volume=%s, 
-					1m_volume=%s,
-					today_high=%s, 
-					today_low=%s ,
-					today_open=%s ,
-					today_close=%s ,
-					today_trades=%s ,
-					bid=%s , 
-					ask=%s ,
-					vwap=%s , 
-					prev_day_open = %s ,
-					prev_day_close = %s , 
-					prev_day_high = %s ,
-					prev_day_low = %s , 
-					prev_day_vwap = %s ,
-					prev_day_volume = %s ,
-					prev_day_trades = %s 
-					where name='%s' """ % (
-								price or 0,
-								dailyBar.get("v") or 0,
-								vol,
-								dailyBar.get("h") or 0,
-								dailyBar.get("l") or 0,
-								dailyBar.get("o") or 0,
-								dailyBar.get("c") or 0,
-								dailyBar.get("n") or 0,
-								latestQuote.get("bp") or 0,
-								latestQuote.get("ap") or 0,
-								minuteBar.get("vw") or 0,
-								prevDailyBar.get("o") or 0,
-								prevDailyBar.get("c") or 0,
-								prevDailyBar.get("h") or 0,
-								prevDailyBar.get("l") or 0,
-								prevDailyBar.get("vw") or 0,
-								prevDailyBar.get("v") or 0,
-								prevDailyBar.get("n") or 0,
-								s )
-					try:
-						frappe.db.sql(sql)
-					except Exception as e:
-						print("error sql",e)
+					# sql = """ update tabSymbol set 
+					# price=%s, 
+					# volume=%s, 
+					# 1m_volume=%s,
+					# today_high=%s, 
+					# today_low=%s ,
+					# today_open=%s ,
+					# today_close=%s ,
+					# today_trades=%s ,
+					# bid=%s , 
+					# ask=%s ,
+					# vwap=%s , 
+					# prev_day_open = %s ,
+					# prev_day_close = %s , 
+					# prev_day_high = %s ,
+					# prev_day_low = %s , 
+					# prev_day_vwap = %s ,
+					# prev_day_volume = %s ,
+					# prev_day_trades = %s 
+					# where name='%s' """ % (
+					# 			price or 0,
+					# 			dailyBar.get("v") or 0,
+					# 			vol,
+					# 			dailyBar.get("h") or 0,
+					# 			dailyBar.get("l") or 0,
+					# 			dailyBar.get("o") or 0,
+					# 			dailyBar.get("c") or 0,
+					# 			dailyBar.get("n") or 0,
+					# 			latestQuote.get("bp") or 0,
+					# 			latestQuote.get("ap") or 0,
+					# 			minuteBar.get("vw") or 0,
+					# 			prevDailyBar.get("o") or 0,
+					# 			prevDailyBar.get("c") or 0,
+					# 			prevDailyBar.get("h") or 0,
+					# 			prevDailyBar.get("l") or 0,
+					# 			prevDailyBar.get("vw") or 0,
+					# 			prevDailyBar.get("v") or 0,
+					# 			prevDailyBar.get("n") or 0,
+					# 			s )
+					# try:
+					# 	frappe.db.sql(sql)
+					# except Exception as e:
+					# 	print("error sql",e)
 
 			except Exception as e:
 				print("error",e)
