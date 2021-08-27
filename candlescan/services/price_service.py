95e948d33dbb43abfe4af1039119d7d93f75799b
ovresko <32199620+ovresko@users.noreply.github.com>
2021-08-27 19:05:16 +0100

    q

--- candlescan/services/price_service.py
+++ candlescan/services/price_service.py
@@ -263,8 +263,8 @@ def backfill(days=0,symbols=None):
 			for result in chunks(symbols,chuck):
 				threads+=1
 				if result:
-					_insert(threads,beg,result)
-					#threading.Thread(target=_insert,args=(threads,beg,result,)).start()	
+					#_insert(threads,beg,result)
+					threading.Thread(target=_insert,args=(threads,beg,result,)).start()	
 			
 			#time.sleep(5 )
 
