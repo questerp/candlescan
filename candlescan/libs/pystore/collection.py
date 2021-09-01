e26f8da01b8c62caac99584b4947bb9f0d041ab6
ovresko <32199620+ovresko@users.noreply.github.com>
2021-09-01 20:45:21 +0100

    q

--- candlescan/libs/pystore/collection.py
+++ candlescan/libs/pystore/collection.py
@@ -102,7 +102,7 @@ class Collection(object):
                 begin
                 INSERT INTO bars_tmp(s,c ,o,h,l,v) select s,c,o,h,l,v from bars where s=NEW.s order by t desc limit 50 ;
                 update ta set 
-                    sma20 = (SELECT sum(c) FROM  bars_tmp)   ,
+                    sma20 = (SELECT sum(c)/20 FROM (SELECT c FROM bars_tmp LIMIT 20)) ,
                     price=NEW.c
                 where s=NEW.s;
                 DELETE FROM bars_tmp;
