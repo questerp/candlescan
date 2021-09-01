fb18a3c25de384b5707b27c570372a93ffd5ddb1
ovresko <32199620+ovresko@users.noreply.github.com>
2021-09-01 12:39:00 +0100

    q

--- candlescan/libs/pystore/collection.py
+++ candlescan/libs/pystore/collection.py
@@ -54,7 +54,7 @@ class Collection(object):
             """
             cur.execute(sql)
             for s in symbols:
-                cur.execute("INSERT INTO ta VALUES(:s)", {'s':s})
+                cur.execute("INSERT INTO ta(s) VALUES(?)", s)
 
 
             
