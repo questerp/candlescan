66e19edb29703733fccd12a6d00afa3b50c329d8
Ovresko <ovresko@gmail.com>
2021-09-02 14:57:48 -0400

    apply_priority

--- candlescan/services/ta_service.py
+++ candlescan/services/ta_service.py
@@ -87,7 +87,7 @@ def ta_snapshot_all(apply_priority=False):
 		all_symbols = get_active_symbols()
 		if apply_priority:
 			if dt.now().minute % 5 != 0:
-				all_symbols = all_symbols[:1000]
+				all_symbols = all_symbols[:2000]
 		for symbols in chunks(all_symbols,500):
 			i+=1
 			t = threading.Thread(target=ta_snapshot,args=(i,symbols,conf,))
