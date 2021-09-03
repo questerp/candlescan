16f6498e2d5668029e2ac2f88ea7cf6978df874f
Ovresko <ovresko@gmail.com>
2021-09-03 07:29:20 -0400

    q

--- candlescan/libs/pystore/item.py
+++ candlescan/libs/pystore/item.py
@@ -69,7 +69,7 @@ class Item(object):
     def today_volume(self):
         sql  = "select sum(v) from bars where s=? and t>=?"
         attrs =[self.item,dt.today().replace(hour=0).timestamp()]
-        data = []
+        data = 0
         conn = apsw.Connection(self.path,flags = apsw.SQLITE_OPEN_READONLY)
         conn.setbusytimeout(2000)
         with conn:
