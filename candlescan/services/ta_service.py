0a6425f3b4631ecc60f741ffd7599f821109d59d
Ovresko <ovresko@gmail.com>
2021-09-03 11:35:11 -0400

    q

--- candlescan/services/ta_service.py
+++ candlescan/services/ta_service.py
@@ -1,5 +1,6 @@
 
 from __future__ import unicode_literals
+from typing import final
 import frappe,time
 from frappe.utils import cstr
 import socketio
@@ -20,8 +21,15 @@ import pymysql
 from pymysql.converters import conversions, escape_string
 import math
 from talib import stream
+import signal
 
+stop_threads = False
+def handler(signum, frame):
+	global stop_threads
+	stop_threads = True
+	print('Ctrl+Z pressed')
 
+signal.signal(signal.SIGTSTP, handler)
 store = pystore.store('bars' )
 collection = store.collection('1MIN' )
 
@@ -219,38 +227,43 @@ ta_func = [
 
 async def run():
 	try:
+		global stop_threads
+
 		await sio.connect('http://localhost:9002',headers={"microservice":"ta_service"})
 		while(1):
-			if dt.now().second != 30:
-				time.sleep(1)
-				continue
-
-			# for symbol in get_active_symbols():
-			if dt.now().minute % 5 == 0:
-				frappe.db.sql("""update tabSymbol set 
-				daily_change_per=ROUND(100*((price - today_open)/today_open),2), 
-				daily_change_val=ROUND((price - today_open),2) 
-				where today_open>0 and price > 0""")
-				frappe.db.commit()
-				time.sleep(1)
-				frappe.db.sql("""update tabSymbol set 
-				daily_close_change_per=ROUND(100*((price - prev_day_close)/prev_day_close),2), 
-				daily_close_change_val=ROUND((price - prev_day_close),2) 
-				where prev_day_close>0 and price > 0""")
-				frappe.db.commit()
-				time.sleep(1)
-				frappe.db.sql("""update tabSymbol set 
-				gap_per=ROUND(100*((price - prev_day_close)/prev_day_close),2), 
-				gap_val=ROUND((price - prev_day_close),2) 
-				where prev_day_close>0 and price > 0""")
-				frappe.db.commit()
-				time.sleep(1)
-
 			try:
-				ta_snapshot_all(True)
-				time.sleep(2)
+				if dt.now().second != 30:
+					time.sleep(1)
+					continue
+
+				# for symbol in get_active_symbols():
+				if dt.now().minute % 5 == 0:
+					frappe.db.sql("""update tabSymbol set 
+					daily_change_per=ROUND(100*((price - today_open)/today_open),2), 
+					daily_change_val=ROUND((price - today_open),2) 
+					where today_open>0 and price > 0""")
+					frappe.db.commit()
+					time.sleep(1)
+					frappe.db.sql("""update tabSymbol set 
+					daily_close_change_per=ROUND(100*((price - prev_day_close)/prev_day_close),2), 
+					daily_close_change_val=ROUND((price - prev_day_close),2) 
+					where prev_day_close>0 and price > 0""")
+					frappe.db.commit()
+					time.sleep(1)
+					frappe.db.sql("""update tabSymbol set 
+					gap_per=ROUND(100*((price - prev_day_close)/prev_day_close),2), 
+					gap_val=ROUND((price - prev_day_close),2) 
+					where prev_day_close>0 and price > 0""")
+					frappe.db.commit()
+					time.sleep(1)
+
+				
+					ta_snapshot_all(True)
+					time.sleep(2)
+
 			except Exception as e:
-				pass
+				print(e)
+				stop_threads = True
 			
 			
 			
@@ -262,7 +275,7 @@ async def run():
 
 def ta_snapshot_all(apply_priority=False):
 	try:
-		ts  = []
+		global stop_threads
 		conf = frappe.conf.copy()
 		i=0
 		all_symbols = []
@@ -276,89 +289,90 @@ def ta_snapshot_all(apply_priority=False):
 			else:
 				all_symbols =  get_active_symbols()[:1000]
 
-			
 		for symbols in chunks(all_symbols,500):
+			if stop_threads:
+				break
 			i+=1
-			t = threading.Thread(target=ta_snapshot,args=(i,symbols,conf,))
-			ts.append(t)
-			t.start()	
+			threading.Thread(target=ta_snapshot,args=(i,symbols,conf,))
+			
 	except KeyboardInterrupt as e:
 		print("error ta_snapshot_all",e)
-		for t in ts:
-			t.raise_exception()
-			t.join()
+		stop_threads = True
 
 
 def ta_snapshot(i,symbols=None,conf=None):
 	start = dt.now()
+	global stop_threads
 	if symbols is None:
 		symbols= get_active_symbols()
 	_cursor = None
 	conn = None
-	if conf:
-		conn = pymysql.connect(
-				user= conf.db_name,
-				password= conf.db_password,
-				database=conf.db_name,
-				host='127.0.0.1',
-				port='',
-				charset='utf8mb4',
-				use_unicode=True,
-				ssl=  None,
-				conv=conversions,
-				local_infile=conf.local_infile
-			)
-		_cursor = conn.cursor()
-	for symbol in symbols:
-		data = collection.item(symbol).snapshot(200,["c","h","l","o","v"]) # [(a,b,...),()...]
-		if data:
-			#print(symbol)
-			close = np.array([v[0] for v in data if v[0]],dtype=np.double)
-			heigh = np.array([v[1] for v in data if v[1]],dtype=np.double)
-			low = np.array([v[2] for v in data if v[2]],dtype=np.double)
-			open = np.array([v[3] for v in data if v[3]],dtype=np.double)
-			volume = np.array([v[4] for v in data if v[4]],dtype=np.double)
-			analysis = {}
-			#t,o,c,h,l,v 
-			for t in ta_func:
-				try:
-					result = calculate_ta(symbol,t,open,close,heigh,low,volume)
-					if result and not math.isnan(result):
-						analysis[t] = result
+	try:
+		if conf:
+			conn = pymysql.connect(
+					user= conf.db_name,
+					password= conf.db_password,
+					database=conf.db_name,
+					host='127.0.0.1',
+					port='',
+					charset='utf8mb4',
+					use_unicode=True,
+					ssl=  None,
+					conv=conversions,
+					local_infile=conf.local_infile
+				)
+			_cursor = conn.cursor()
+		for symbol in symbols:
+			if stop_threads:
+				break
+			data = collection.item(symbol).snapshot(200,["c","h","l","o","v"]) # [(a,b,...),()...]
+			if data:
+				#print(symbol)
+				close = np.array([v[0] for v in data if v[0]],dtype=np.double)
+				heigh = np.array([v[1] for v in data if v[1]],dtype=np.double)
+				low = np.array([v[2] for v in data if v[2]],dtype=np.double)
+				open = np.array([v[3] for v in data if v[3]],dtype=np.double)
+				volume = np.array([v[4] for v in data if v[4]],dtype=np.double)
+				analysis = {}
+				#t,o,c,h,l,v 
+				for t in ta_func:
+					try:
+						result = calculate_ta(symbol,t,open,close,heigh,low,volume)
+						if result and not math.isnan(result):
+							analysis[t] = result
 
-				except Exception as e:
-					print("ERROR TA",e)
-			if _cursor and analysis:
-				fields = [field for field in analysis.keys() ] + [""]
-				args = ("=ROUND(%s, 2), ".join(fields))
-				args = args[:-2]
-				#print(args)
-				#print(tuple([analysis[t] for t in ta_func]))
-				fargs= args % tuple([ val  for val in  analysis.values() ])
-				#print(fargs)
+					except Exception as e:
+						print("ERROR TA",e)
+				if _cursor and analysis:
+					fields = [field for field in analysis.keys() ] + [""]
+					args = ("=ROUND(%s, 2), ".join(fields))
+					args = args[:-2]
+					#print(args)
+					#print(tuple([analysis[t] for t in ta_func]))
+					fargs= args % tuple([ val  for val in  analysis.values() ])
+					#print(fargs)
 
-				sql = """  update tabIndicators set 
-						%s
-						where symbol='%s'
-				 """ %  (fargs,symbol)
-				#print(sql)
-				try:
-					sql = str(sql)
-					_cursor.execute(sql)
-					
-				except Exception as e:
-					print("error sql",e,sql)
-
-
-
-	
-	if conn:
-		_cursor.execute("COMMIT;")
-		conn.close()
-		_cursor = None
-		conn = None		
-	end = dt.now()
-	print(i,"DONE",end-start)
+					sql = """  update tabIndicators set 
+							%s
+							where symbol='%s'
+					""" %  (fargs,symbol)
+					#print(sql)
+					try:
+						sql = str(sql)
+						_cursor.execute(sql)
+						
+					except Exception as e:
+						print("error sql",e,sql)
+	except Exception as e:
+		print("error ta_snapshot",e)
+	finally:
+		if conn:
+			_cursor.execute("COMMIT;")
+			conn.close()
+			_cursor = None
+			conn = None		
+		end = dt.now()
+		print(i,"DONE",end-start)
 
 
 @sio.event
