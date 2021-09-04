dcd3c6e1792fa79a513176f9bd2ad44e04e40ea5
Ovresko <ovresko@gmail.com>
2021-09-04 17:01:38 -0400

    q

--- candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.js
+++ candlescan/candlescan/doctype/candlescan_settings/candlescan_settings.js
@@ -6,27 +6,12 @@ frappe.ui.form.on('Candlescan Settings', {
 		frm.add_custom_button("Commit",
 			() => {
 				frappe.call({
-					method: 'candlescan.candlescan.doctype.candlescan_settings.commit',
+					method: 'candlescan.candlescan.doctype.candlescan_settings.candlescan_settings.commit',
 					args: { },
 					callback(r) {
 						alert("commit");
 					}
 				});
 			});
-		 frm.add_custom_button("Start services",
-			() => {
-				frappe.call({
-					method: 'candlescan.candlescan_service.start_services',
-					args: { },
-					callback(r) {
-						console.log(r);
-						alert("services running",r.message);
-						if (!r.exc) {
-								frm.reload_doc();
-							}
-					}
-				});
-			});
-		 
 	 }
 });
